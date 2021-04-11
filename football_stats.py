1d9644c44a630f22dc77f2ff21195e323e2da6da
dailyfatcat <50534920+dailyfatcat@users.noreply.github.com>
2021-04-11 15:53:39 -0400

    Assignment 9

--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,36 @@
+#import statements
+from bs4 import BeautifulSoup
+import urllib3
+
+
+def main():
+
+    '''Write a script called “football_stats.py” that will load this URL,
+    parse it using BeautiulSoup,
+    and output the list of top 20 players, including the player’s position, team and total number of touchdowns'''
+
+    http = urllib3.PoolManager()
+    response = http.request('GET',
+                            'https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/?sortcol=td&sortdir=descending')
+    soup = BeautifulSoup(response.data, features='lxml')
+    table = soup.find_all('tr', attrs={'class': 'TableBase-bodyTr'})
+
+    counter = 0
+    for rows in table:
+        player_name = rows.find('span', attrs={'class': 'CellPlayerName--long'}).find('a').get_text()
+        player_team = rows.find('span', attrs={'class': 'CellPlayerName-team'}).get_text().strip()
+        player_position = rows.find('span', attrs={'class': 'CellPlayerName-position'}).get_text().strip()
+        player_td = rows.find('td', attrs={'class': 'TableBase-bodyTd--number'}).get_text().strip()
+
+        print("Name: {}".format(player_name))
+        print("Team: {}".format(player_team))
+        print("Positions: {}".format(player_position))
+        print("Touchdowns: {}".format(player_td))
+        print("\n")
+
+        counter += 1
+        if counter > 19:
+            break
+
 if __name__ == "__main__":
-    pass
+    main()
--- nfl_spreads.py
+++ nfl_spreads.py
@@ -1,2 +1,29 @@
+#import statements
+from bs4 import BeautifulSoup
+import urllib3
+
+
+def main():
+    '''2.	Write a script called “nfl_spreads.py” that will load this URL,
+    # parse it using BeautifulSoup,
+    and output the favorite, the underdog and the spread of a given game.'''
+
+    http = urllib3.PoolManager()
+    response = http.request('GET', 'http://www.footballlocks.com/nfl_point_spreads.shtml')
+    soup = BeautifulSoup(response.data, features='lxml')
+    table = soup.find('table', attrs={'cols': '4'})
+
+    rows = table.find_all('tr')
+
+    for row in rows:
+        teams = row.find_all('td')
+        #use a try statement in the case that the data is not well formatted
+        #in this webpage the data is well formatted
+        try:
+            print(f'{teams[0].get_text()} |  {teams[1].get_text():20} | {teams[2].get_text():10} |  {teams[3].get_text():10}')
+        except:
+            print("invalid")
+            continue
+
 if __name__ == "__main__":
-    pass
+    main()
